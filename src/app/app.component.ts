53e98d0d4c72d2417adf912f69588aca647ff0f8
PrayasD <prayasdhand13@gmail.com>
2024-02-22 12:05:28 +0000

    First Commint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ChatApplication';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    window.onload = () => {
+      const header = document.getElementById('myHeader');
+      const page = document.getElementById('page');
+      const openMenuButton = document.getElementById('openmenu');
+  
+      window.addEventListener('scroll', () => {
+        page?.classList.remove('menuopen');
+        if (window.scrollY >= 100) {
+          header?.classList.add('sticky');
+        } else {
+          header?.classList.remove('sticky');
+        }
+      });
+  
+      openMenuButton?.addEventListener('click', () => {
+        header?.classList.remove('sticky');
+        page?.classList.add('menuopen');
+      });
+  
+      const links = document.querySelectorAll('a[href^="#"]');
+  
+      links.forEach((link: Element) => {
+        const linkElement = link as HTMLElement;
+        linkElement.addEventListener('click', (event) => {
+          event.preventDefault();
+          const targetId = linkElement.getAttribute('href');
+          const targetElement = document.querySelector(targetId!);
+          if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+          }
+        });
+      });
+    };
+  }
+  
 }
